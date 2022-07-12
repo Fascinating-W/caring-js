afcad955ec3049f5de7a8ba06795255c80e9e20d
风度万人迷 <805391114@qq.com>
2022-07-12 14:04:06 +0800

    version:0.0.1

---
+++ src/caring-js/index.js
@@ -0,0 +1,23 @@
+
+export {debounce} from "./js/debounce"
+export {throttle} from './js/throttle'
+import is from './js/test'
+export { is}
+export {
+  range,
+  sleep,
+  random,
+  guid,
+  deepClone,
+  deepMerge,
+  randomArray,
+  timeFormat,
+  timeFrom,
+  trim,
+  queryParams,
+  getParams,
+  priceFormat,
+  padZero,
+  getProperty,
+  setProperty
+} from './js'
---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { is } from '../../caring-js'
+export default {
+  created() {
+
+    let e = '123465798@gmail.com'
+
+    console.log(is.email(e))
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
